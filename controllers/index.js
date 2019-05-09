6c9f006b4404e7cec13cc980f4a6f585d1d17180
Jake Mulley <jakemulley@users.noreply.github.com>
2019-05-09 13:56:33 +0100

    add schema output

--- controllers/index.js
+++ controllers/index.js
@@ -1,39 +1,38 @@
-const csvtojson = require('csvtojson');
-const AJV = require('ajv');
+const csvtojson = require('csvtojson')
+const AJV = require('ajv')
 const ajv = new AJV({
   allErrors: true,
   verbose: true
-});
-const validate = ajv.compile(require('./../schema.json'));
-const fs = require('fs');
+})
+const validate = ajv.compile(require('./../schema.json'))
 
 const controller = {
-  index(req, res) {
-    return res.render('index', { schema: JSON.stringify(require('./../schema.json'), null, 2) });
+  index (req, res) {
+    return res.render('index', { schema: JSON.stringify(require('./../schema.json'), null, 2) })
   },
-  validate(req, res) {
+  validate (req, res) {
     csvtojson().fromFile(req.file.path).then(jsonObj => {
       const results = jsonObj.filter(item => Object.values(item).some(result => result.length)).map(item => {
-        item.GeoX = isNaN(item.GeoX) ? item.GeoX : +item.GeoX;
-        item.GeoY = isNaN(item.GeoY) ? item.GeoY : +item.GeoY;
-        item.Hectares = isNaN(item.Hectares) ? item.Hectares : +item.Hectares;
-        item.MinNetDwellings = isNaN(item.MinNetDwellings) ? item.MinNetDwellings : +item.MinNetDwellings;
-        item.NetDwellingsRangeFrom = isNaN(item.NetDwellingsRangeFrom) ? item.NetDwellingsRangeFrom : +item.NetDwellingsRangeFrom;
-        item.NetDwellingsRangeTo = isNaN(item.NetDwellingsRangeTo) ? item.NetDwellingsRangeTo : +item.NetDwellingsRangeTo;
+        item.GeoX = isNaN(item.GeoX) ? item.GeoX : +item.GeoX
+        item.GeoY = isNaN(item.GeoY) ? item.GeoY : +item.GeoY
+        item.Hectares = isNaN(item.Hectares) ? item.Hectares : +item.Hectares
+        item.MinNetDwellings = isNaN(item.MinNetDwellings) ? item.MinNetDwellings : +item.MinNetDwellings
+        item.NetDwellingsRangeFrom = isNaN(item.NetDwellingsRangeFrom) ? item.NetDwellingsRangeFrom : +item.NetDwellingsRangeFrom
+        item.NetDwellingsRangeTo = isNaN(item.NetDwellingsRangeTo) ? item.NetDwellingsRangeTo : +item.NetDwellingsRangeTo
 
-        let valid = validate(item);
-        item.errors = [];
+        let valid = validate(item)
+        item.errors = []
 
-        if(!valid) {
-          item.errors = validate.errors;
+        if (!valid) {
+          item.errors = validate.errors
         }
 
-        return item;
-      });
+        return item
+      })
 
-      return res.render('validate', { results });
-    });
+      return res.render('validate', { results })
+    })
   }
 }
 
-module.exports = controller;
+module.exports = controller
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const app = express();
-const index_controller = require('./controllers/index.js');
-const multer = require('multer');
-const upload = multer({ dest: 'uploads' });
+const express = require('express')
+const multer = require('multer')
+const controller = require('./controllers/index.js')
+const app = express()
+const upload = multer({ dest: 'uploads' })
+const port = process.env.PORT || 5000
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
-app.get('/', index_controller.index);
-app.post('/', upload.single('csv_file'), index_controller.validate);
+app.get('/', controller.index)
+app.post('/', upload.single('csv_file'), controller.validate)
 
-app.listen(process.env.PORT || 5000, () => console.log('App running on :3000'));
+app.listen(port, () => console.log(`App running on ${port}`))
